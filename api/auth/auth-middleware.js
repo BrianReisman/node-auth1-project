a995859d43d1a8238d615b0ec83cf272958c2cd7
BrianReisman <brianreisman@protonmail.com>
2021-04-07 22:38:34 -0400

    mvp met. final commit

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-const helpers = require('../users/users-model')
+const helpers = require("../users/users-model");
 
 const restricted = (req, res, next) => {
   if (req.session.user) {
@@ -8,18 +8,10 @@ const restricted = (req, res, next) => {
   }
 };
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 const checkUsernameFree = async (req, res, next) => {
-  const check = await helpers.findBy({username: req.body.username})
-  if(check[0]){
-    res.status(400).json({message: 'username taken'})
+  const check = await helpers.findBy({ username: req.body.username });
+  if (check[0]) {
+    res.status(400).json({ message: "username taken" });
   } else {
     next();
   }
@@ -33,21 +25,22 @@ const checkUsernameFree = async (req, res, next) => {
     "message": "Invalid credentials"
   }
 */
-const checkUsernameExists = (req, res, next) => {
-  console.log("checkUsernameExists");
-  next();
-};
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+const checkUsernameExists = async (req, res, next) => {
+  console.log(req.body.username);
+  const exists = await helpers.findBy({ username: req.body.username }).first();
+  if (exists) {
+    next();
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
   }
-*/
+};
+
 const checkPasswordLength = (req, res, next) => {
-  console.log("checkPasswordLength");
-  next();
+  if (req.body.password.length < 4) {
+    res.status(422).json({ message: "Password must be longer than 3 chars" });
+  } else {
+    next();
+  }
 };
 
 module.exports = {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const {
 checkUsernameFree,
-// checkUsernameExists,
-// checkPasswordLength,
+checkUsernameExists,
+checkPasswordLength,
 } = require("./auth-middleware");
 
 const bcrypt = require("bcryptjs"); //package for hashing
 const Users = require("../users/users-model"); //helper functions
 
-router.post("/register", checkUsernameFree, async (req, res, next) => {
+router.post("/register", checkUsernameFree, checkPasswordLength, async (req, res, next) => {
   const user = req.body;
   const hash = bcrypt.hashSync(user.password, 8);
   user.password = hash;
@@ -21,13 +21,12 @@ router.post("/register", checkUsernameFree, async (req, res, next) => {
   }
 });
 
-router.post("/login", async (req, res, next) => {
+router.post("/login", checkUsernameExists, async (req, res, next) => {
   const { username, password } = req.body;
   try {
     const user = await Users.findBy({ username }).first();
     if (user && bcrypt.compareSync(password, user.password)) {
       req.session.user = user;
-      console.log(req.session)
       res.status(200).json({ message: `Welcome ${user.username}!` });
     } else {
       res.status(401).json({ message: "invalid credentials" });
